e3dd5b083440d01cd0906aa344917941089e651e
Pika-Pool <33688622+Pika-Pool@users.noreply.github.com>
2020-12-26 18:48:52 +0530

    add html attributes to django form elements

--- main/forms.py
+++ main/forms.py
@@ -1,7 +1,20 @@
-from django.forms import ModelForm
+from django import forms
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 
+# class CreateUserForm(UserCreationForm):
+#     model = User
+#     fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
+
 class CreateUserForm(UserCreationForm):
-    model = User
-    fields = ['username', 'email', 'password1', 'password2']
+    first_name = forms.fields.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'First Name'}), max_length=32, help_text='First name')
+    last_name=forms.fields.CharField(widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Last Name'}), max_length=32, help_text='Last name')
+    email=forms.fields.EmailField(widget=forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}), max_length=64, help_text='Enter a valid email address')
+    password1=forms.fields.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password'}))
+    password2=forms.fields.CharField(widget=forms.PasswordInput(attrs={'class': 'form-control', 'placeholder': 'Password Again'}))
+
+    class Meta(UserCreationForm.Meta):
+        model = User
+        # I've tried both of these 'fields' declaration, result is the same
+        # fields = ('username', 'first_name', 'last_name', 'email', 'password1', 'password2', )
+        fields = ['first_name', 'last_name', 'email', 'password1', 'password2']
