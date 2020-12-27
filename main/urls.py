4ae6b3db052171d3ce6d2d0aad3781d506fd6d08
Rohit Bisht <rohitbst.104764@gmail.com>
2020-12-27 09:53:05 +0530

    [Patient, Doctors Views]

--- main/urls.py
+++ main/urls.py
@@ -4,7 +4,14 @@ from django.urls import path
 urlpatterns = [
     path('', views.homePage, name='home'),
     path('patient/', views.patientPage, name="patient"),
+
     path('register/', views.registerPage, name='register'),
     path('login/', views.loginPage, name="login"),
     path('logout/', views.logoutPage, name="logout"),
+
+    path('create_appointment/<str:pk>', views.create_appointment, name="create_appointment"),
+    path('update_apoint/<str:pk>/', views.update_appointment, name="update_appoint"),
+    path('delete_appoint/<str:pk>/', views.delete_appointment, name="delete_appoint"),
+
+    path('prescription/<str:pk>', views.prescription, name="prescription"),
 ]
